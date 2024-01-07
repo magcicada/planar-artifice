48fd9e8bfb0882890a99ac503aca71d7655bb492
magcicada <l1334938030@gmail.com>
2024-01-07 21:53:12 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,6 @@ fancyGradle {
 }
 
 repositories {
-    maven { url = 'https://maven.minecraftforge.net/' }
     maven {
         name = "Cursemaven"
         url = uri("https://www.cursemaven.com")
@@ -112,7 +111,7 @@ repositories {
 }
 
 dependencies {
-    minecraft 'net.minecraftforge:forge:1.12.2-14.23.5.2847'
+    minecraft(group = "net.minecraftforge", name = "forge", version = "${versionMc}-${versionForge}")
     implementation(fg.deobf(group = "mezz.jei", name = "jei_${versionMc}", version = versionJEI))
     implementation(group = "com.azanor.baubles", name = "Baubles", version = "${versionMcMajor}-${versionBaubles}")
     implementation(fg.deobf(group = "curse.maven", name = "thaumcraft-223628", version = versionThaumcraft))
